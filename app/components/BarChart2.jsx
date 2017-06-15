b5c213aaf6c14d421854337620d9fbbcf9b2e9bf
bryan <bryan@localhost.localdomain>
2017-06-15 12:08:50 -0500

    bar graph complete

--- app/components/BarChart2.jsx
+++ app/components/BarChart2.jsx
@@ -9,63 +9,36 @@ var staticSize = [500,500];
 export default class BarChart extends React.Component {
   constructor(props){
     super(props)
-    this.createBarChart = this.createBarChart.bind(this)
   }
-  componentDidMount() {
-    this.createBarChart()
-  }
-  componentDidUpdate() {
-    this.createBarChart()
-  }
-  createBarChart() {
-    const node = this.node
-    const dataMax = max(staticData) //sets alias for max value
+
+  render() {
+    const dataMax = Math.max(...staticData) //sets alias for max value
     // convenience function to scale y variable within the bounds 
     // of the <svg /> element
     const yScale = scaleLinear()
       .domain([0, dataMax])
       .range([0, staticSize[1]])
 
-    //on enter, create placeholder DOM elements to hold the data
-    // .enter() returns placeholder nodes for each datam that had
-    // no corresponding DOM element in the selection.
-    // typically used to add new nodes
-    // e.g. in a set [1,2,3,4] with existing DIV nodes corresponding
-    // to 1 and 2, a d3.selectAll('div').data([1,2,3,4]).enter()
-    // will return DOM placeholder elements corresponding to 3 and 4
-    select(node)
-      .selectAll('rect')
-      .data(staticData)
-      .enter()
-      .append('rect')
-   
-    // trim away excess DOM elements not in the dataset
-    // .exit() selects all elements that do not match the dataset
-    // i.e. when the data changes, remove extraneous elements
-    select(node)
-      .selectAll('rect')
-      .data(staticData)
-      .exit()
-      .remove()
-     
-    // add attributes to remaining DOM elements
-    select(node)
-      .selectAll('rect')
-      .data(staticData)
-      .style('fill', '#fe9922')
-      .attr('x', (d,i) => i * 25)
-      .attr('y', d => staticSize[1] - yScale(d))
-      .attr('height', d => yScale(d))
-      .attr('width', 25)
-  }
-
-  render() {
     return (
-      <svg
-        ref={node => this.node = node}
-        width={500} height={500}>
+      <svg width="500" height="500">
+        {staticData.map((d,i)=>
+          <Rect
+            key={i}
+            style={{fill: "#fe9922"}}
+            x={i*25}
+            y={staticSize[1] - yScale(d)}
+            height={yScale(d)}
+            width="25" />
+        )}
       </svg>
     )
   }
+}
+
+const Rect = ({x,y,height,width,style}) => {
+
+  return (
+    <rect x={x} y={y} height={height} width={width} style={style} />
+  );
 }
 
