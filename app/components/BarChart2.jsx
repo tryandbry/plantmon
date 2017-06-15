f74b6bde389677dc46c50ad121f98c55fc9a4715
bryan <bryan@localhost.localdomain>
2017-06-15 17:22:05 -0500

    re-renders with setInterval

--- app/components/BarChart2.jsx
+++ app/components/BarChart2.jsx
@@ -3,15 +3,33 @@ import { scaleLinear } from 'd3-scale';
 import { max } from 'd3-array';
 import { select } from 'd3-selection';
 
-var staticData = [5,10,1,3];
+//var staticData = [5,10,1,3];
 var staticSize = [500,500];
+
+const randomInt = (n)=>Math.round(Math.random()*n);
 
 export default class BarChart extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
+
+    this.state = {
+      data: [5,10,1,3],
+    };
+  }
+  
+  componentDidMount(){
+    this.timeoutId = setInterval(()=>this.setState({
+      data: [
+        randomInt(10),
+        randomInt(10),
+        randomInt(10),
+        randomInt(10),
+      ],
+    }),500);
   }
 
   render() {
+    const staticData = this.state.data;
     const dataMax = Math.max(...staticData) //sets alias for max value
     // convenience function to scale y variable within the bounds 
     // of the <svg /> element
@@ -20,17 +38,19 @@ export default class BarChart extends React.Component {
       .range([0, staticSize[1]])
 
     return (
-      <svg width="500" height="500">
-        {staticData.map((d,i)=>
-          <Rect
-            key={i}
-            style={{fill: "#fe9922"}}
-            x={i*25}
-            y={staticSize[1] - yScale(d)}
-            height={yScale(d)}
-            width="25" />
-        )}
-      </svg>
+      <div>
+        <svg width="500" height="500">
+          {staticData.map((d,i)=>
+            <Rect
+              key={i}
+              style={{fill: "#fe9922"}}
+              x={i*25}
+              y={staticSize[1] - yScale(d)}
+              height={yScale(d)}
+              width="25" />
+          )}
+        </svg>
+      </div>
     )
   }
 }
