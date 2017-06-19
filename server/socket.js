1df5e7836f0c6b87c00f6081966c16ac5c1f4768
bryan <bryan@localhost.localdomain>
2017-06-19 10:34:51 -0500

    adds server-side buffer for emitting tessel data

--- server/socket.js
+++ server/socket.js
@@ -4,14 +4,21 @@ module.exports = (io)=>{
   io.on('connection',(socket)=>{
     console.log('a user connected');
     socket.on('disconnect',()=>console.log('user disconnected'));
+    let buffer = [];
+    const INTERVAL=700;
     setInterval(()=>{
       axios.get('http://172.28.116.169')
       //axios.get('http://localhost:1337/api/garbage')
       .then(payload=>{
-        console.log('tessel data:',payload.data);
-        socket.emit('tessel',{data: payload.data});
+        buffer.push(payload.data);
+        //console.log('tessel data:',payload.data);
+        console.log('buffer',buffer);
+        //socket.emit('tessel',{data: payload.data});
       })
       .catch(err=>console.error(err));
-    },1000);
+    },INTERVAL);
+    setInterval(()=>{
+      if(buffer.length) socket.emit('tessel',{data: buffer.shift()});
+    },INTERVAL);
   });
 }
